1e08caf6a4a5adf0d08ea8abf68f9c978b4898eb
wVim-design <1298304899@qq.com>
2020-07-28 12:29:46 +0800

    加了第一次注释

--- src/main/java/fileSys/aspect/Journal.java
+++ src/main/java/fileSys/aspect/Journal.java
@@ -6,7 +6,7 @@ import java.lang.annotation.*;
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 public @interface Journal {
-
+//这是注释
 //        public String process() default "正常";
 
         public String event() default "";
